1346ba961bef429be39dd8251229d8b6ba155530
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-09-22 13:38:52 -0400

    move prompts and markdown template to ai_changelog/string_templates.py

---
+++ ai_changelog/string_templates.py
@@ -0,0 +1,21 @@
+sys_msg = """
+You are a senior developer tasked with code review and devops.
+You are reviewing commits from a junior developer.
+You want to demonstrate how to craft meaningful descriptions that are concise and easy to understand.
+Interpret the commit and diff messages below to create descriptions for each commit.
+"""
+
+
+hum_msg = """
+{commit_hash}
+----------------
+{diff}
+"""
+
+
+markdown_template = """
+## {short_description}
+{commit_hash}
+----------------
+{bullet_points}
+"""
