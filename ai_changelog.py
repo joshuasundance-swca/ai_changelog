3d899220ba6b00bff8bf4eaa602b2f92a9c652d1
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-09-22 13:42:10 -0400

    move pydantic models to ai_changelog/pydantic_models.py

--- ai_changelog.py
+++ ai_changelog.py
@@ -1,35 +1,15 @@
 import os
 import subprocess
-from typing import Optional, List
+from typing import Optional
 
 from langchain.chains.openai_functions import (
     create_structured_output_chain,
 )
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
-from langchain.pydantic_v1 import BaseModel, Field
 
-from ai_changelog.string_templates import sys_msg, hum_msg, markdown_template
-
-
-class Commit(BaseModel):
-    commit_hash: str = Field(..., description="The commit hash")
-    diff: str = Field(..., description="The diff for the commit")
-
-
-class CommitDescription(BaseModel):
-    short_description: str = Field(..., description="A technical and concise description of changes implemented in the commit")
-    long_description: List[str] = Field(..., description="Markdown bullet-point formatted list of changes implemented in the commit")
-
-
-class CommitInfo(Commit, CommitDescription):
-    def markdown(self):
-        bullet_points = "\n".join([f"- {line.strip('*- ')}" for line in self.long_description])
-        return markdown_template.format(
-            short_description=self.short_description,
-            commit_hash=self.commit_hash,
-            bullet_points=bullet_points
-        )
+from ai_changelog.pydantic_models import CommitDescription, CommitInfo, Commit
+from ai_changelog.string_templates import sys_msg, hum_msg
 
 
 def get_commits(
