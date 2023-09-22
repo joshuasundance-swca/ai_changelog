37a68c67c416099765f363a10312ae114cc5ea72
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-09-22 15:58:51 -0400

    --no-notes

--- ai_changelog/utils.py
+++ ai_changelog/utils.py
@@ -42,6 +42,7 @@ def get_commits(
                 "git",
                 "--no-pager",
                 "show",
+                "--no-notes",
                 commit,
                 "-s",
                 "--pretty=%cd",
