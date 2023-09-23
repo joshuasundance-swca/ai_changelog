9b191a4b7969a84cd73558107df4a1670f468582
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-09-23 11:40:27 -0400

    move action to push; scriptify

--- ai_changelog/update.py
+++ ai_changelog/update.py
@@ -1,11 +1,18 @@
+import argparse
 import os
 import subprocess
+from typing import Optional
 
 from pydantic_models import Commit, CommitDescription
 from utils import get_commits, get_descriptions, infos_to_str
 
 
-def main():
+def main(
+    repo_path: Optional[str] = None,
+    base_ref: str = "origin/main",
+    head_ref: str = "HEAD",
+    context_lines: int = 5,
+):
     # Check to see if AI_CHANGELOG.md already exists
     if os.path.isfile("AI_CHANGELOG.md"):
         # If so, restore the original version from main
@@ -16,7 +23,12 @@ def main():
             existing_content = "\n".join(existing_changelog.readlines()[2:]).strip()
 
     # Generate the new AI_CHANGELOG.md
-    new_commits: list[Commit] = get_commits()
+    new_commits: list[Commit] = get_commits(
+        repo_path=repo_path,
+        base_ref=base_ref,
+        head_ref=head_ref,
+        context_lines=context_lines,
+    )
     new_commit_infos: list[CommitDescription] = get_descriptions(new_commits)
     new_descriptions: str = infos_to_str(new_commit_infos).strip()
 
@@ -31,4 +43,37 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Process command line arguments.")
+    parser.add_argument(
+        "--repo_path",
+        type=str,
+        default=None,
+        help="Path to the repository",
+    )
+    parser.add_argument(
+        "--base_ref",
+        type=str,
+        default="origin/main",
+        help="Base reference for commits",
+    )
+    parser.add_argument(
+        "--head_ref",
+        type=str,
+        default="HEAD",
+        help="Head reference for commits",
+    )
+    parser.add_argument(
+        "--context_lines",
+        type=int,
+        default=5,
+        help="Number of context lines for each commit",
+    )
+
+    args = parser.parse_args()
+
+    main(
+        repo_path=args.repo_path,
+        base_ref=args.base_ref,
+        head_ref=args.head_ref,
+        context_lines=args.context_lines,
+    )
