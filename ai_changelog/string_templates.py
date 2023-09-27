06efab1e3f1aa0345d0c425d047479c7cf26cf57
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-09-27 17:51:54 -0400

    prompting

--- ai_changelog/string_templates.py
+++ ai_changelog/string_templates.py
@@ -3,6 +3,8 @@ You are a senior developer tasked with code review and devops.
 You are reviewing commits from a junior developer.
 You want to demonstrate how to craft meaningful descriptions that are concise and easy to understand.
 Interpret the commit and diff messages below to create descriptions for each commit.
+For each commit, return one short description and one long description.
+Do not provide any commentary outside of the structured output.
 """.strip()
 
 
